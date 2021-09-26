37330bfdc0245a5055c2e2bd32c70afc5579a1c4
sabbir-hossai <sabbirab3@gmail.com>
2021-09-26 11:53:54 +0600

    first commit

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className="header">
+            <h1>Blood donor society</h1>
+            <h3>This year budget 5 million.</h3>
+        </div>
+    );
+};
+
+export default Header;
