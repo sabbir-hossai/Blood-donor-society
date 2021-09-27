526bc71d99d85b97c057cea14aa0dca985165d1f
sabbir-hossai <sabbirab3@gmail.com>
2021-09-27 16:20:40 +0600

    commpied

--- src/Components/Donors/Donor/Donor.js
+++ src/Components/Donors/Donor/Donor.js
@@ -1,25 +1,28 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee, faHardHat, faHeart } from '@fortawesome/free-solid-svg-icons'
 import './Donor.css';
 
 const Donor = (props) => {
     // console.log(props.donor)
 
     const { name, id, img, bloodDonate, profession, age, counry } = props.donor;
+    const element = <FontAwesomeIcon icon={faHeart} />
     return (
-        <div class="row ">
-            <div class="col">
-                <div class="card ab">
-                    <div class="img-container">
-                        <img src={img} class="card-img-top" alt="..." />
+        <div className="row ">
+            <div className="col">
+                <div className="card ab">
+                    <div className="img-container">
+                        <img src={img} className="card-img-top" alt="..." />
                     </div>
-                    <div class="card-body">
-                        <h5 style={{ textAlign: 'center' }} class="card-title">{name}</h5>
-                        <p class="card-text"><small>Age:{age}</small> </p>
-                        <p class="card-text"><small>Country:{counry}</small> </p>
-                        <p class="card-text"><small>Doner id:{id}</small> </p>
-                        <p class="card-text"><small>BloodDonate: {bloodDonate}</small> </p>
-                        <p class="card-text"><small>Profession: {profession}</small> </p>
-                        <button onClick={() => props.bloodDonateHandel(props.donor)} class="btn btn-primary" >Blood donate</button>
+                    <div className="card-body">
+                        <h5 style={{ textAlign: 'center' }} className="card-title">{name}</h5>
+                        <p className="card-text"><small>Age:{age}</small> </p>
+                        <p className="card-text"><small>Country:{counry}</small> </p>
+                        <p className="card-text"><small>Doner id:{id}</small> </p>
+                        <p className="card-text"><small>BloodDonate: {bloodDonate}</small> </p>
+                        <p className="card-text"><small>Profession: {profession}</small> </p>
+                        <button onClick={() => props.bloodDonateHandel(props.donor)} className="btn btn-primary" >{element} Blood donate</button>
                     </div>
                 </div>
             </div>
--- src/Components/Donors/Donors.js
+++ src/Components/Donors/Donors.js
@@ -1,16 +1,26 @@
 import React, { useEffect, useState } from 'react';
+
+
 import Donor from './Donor/Donor';
 import DonorInfo from './Donor/DonorInfo';
 import './Donors.css';
 
 const Donors = () => {
+
+
     const [donors, setDonors] = useState([]);
     // blood donor count
     const [donorsCount, setDonorsCount] = useState("");
     // total blood donation
     const [totalDonation, setTotalDonation] = useState(0);
+
+
+
     // / blood donorsInfo
-    const [donorsInfo, setDonorsInfo] = useState("");
+    const [a, setA] = useState([])
+    const [donorName, setDonorName] = useState([])
+
+
 
     useEffect(() => {
         fetch('./Donor.json')
@@ -20,6 +30,7 @@ const Donors = () => {
 
 
     const bloodDonateHandel = donors => {
+        const { name } = donors;
         // blood donor count
         const showDonor = [...donorsCount, donors.name];
         setDonorsCount(showDonor)
@@ -27,13 +38,24 @@ const Donors = () => {
         const totalDonotion = [JSON.parse(donors.bloodDonate) + JSON.parse(totalDonation)];
         setTotalDonation(totalDonotion)
 
-        // blood donorsInfo
-        const showDonorsInfo = [...donorsInfo, donors.name];
-        setDonorsInfo(showDonorsInfo)
 
-        console.log(totalDonation)
+        // blood donorsInfo
+        let showA = a;
+        if (!showA) {
+            showA = name;
+        } else {
+            showA = [...showA, name];
+        }
+        setA(showA)
+        let x = [""]
+        for (const b of showA) {
+            x = [...x, b];
+
+        }
+        setDonorName(x)
 
     }
+
     return (
         <div className="donors">
             <div className="donor">
@@ -43,10 +65,12 @@ const Donors = () => {
                         bloodDonateHandel={bloodDonateHandel}></Donor>)
                 }
             </div>
-            <div className="">
+            <div className="donors-cart">
                 <h3>Blood donor:{donorsCount.length}</h3>
                 <h3>total blood donation:{totalDonation}</h3>
-                <h6 className="donorsInfo">{donorsInfo}</h6>
+                <h6>{donorName}</h6>
+
+
             </div>
             <div>
 
